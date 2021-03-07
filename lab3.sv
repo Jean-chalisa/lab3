daaeaa45a4e600f310999ab8957a3896ca653066
David Wang <dwang3@andrew.cmu.edu>
2021-03-08 00:03:42 +0800

    latest changes

--- lab3.sv
+++ lab3.sv
@@ -2,17 +2,16 @@
 
 module SECDEDdecoder (input  logic [12:0] inCode,
                       output logic [3:0] syndrome,
-                      output logic is1BitErr, is2BitErr, 
+                      output logic is1BitErr, is2BitErr,
                       output logic [12:0] outCode);
 
 
-makeSyndrome dut1 (.codeWord(inCode), .*);
+  makeSyndrome dut1 (.codeWord(inCode), .*);
 
-makeCorrect dut2 ( .codeWord(inCode), .syndrome(syndrome),
-                   .correctCode(corrected));
+  makeCorrect dut2 ( .codeWord(inCode), .syndrome(syndrome),
+                   .correctCodeWord(outCode));
 
-errorCheck dut2 ( .inCode(.*), .syndrome(.*), .1BitErr(.*), 2BitErr(.*))
-
+  errorCheck dut3 (.*);
 
 
 endmodule:  SECDEDdecoder
@@ -23,16 +22,16 @@ module makeSyndrome (input  logic [12:0] codeWord,
 
   assign syndrome[0] = codeWord[1] ^ codeWord[3] ^ codeWord[5] ^ codeWord[7] ^
                        codeWord[9] ^ codeWord[11];
-                       
+
   assign syndrome[1] = codeWord[2] ^ codeWord[3] ^ codeWord[6] ^ codeWord[7] ^
-                       codeWord[10] ^ codeWord[11]
+                       codeWord[10] ^ codeWord[11];
 
   assign syndrome[2] = codeWord[4] ^ codeWord[5] ^ codeWord[6] ^ codeWord[7] ^
                        codeWord[12];
-                       
+
   assign syndrome[3] = codeWord[8] ^ codeWord[9] ^ codeWord[10] ^ codeWord[11] ^
                        codeWord[12];
-                       
+
 
 endmodule: makeSyndrome
 
@@ -53,23 +52,21 @@ module makeCorrect (input  logic [12:0] codeWord,
     correctCodeWord[syndrome] = ~codeWord[syndrome];
 
   else if (PGfail === 1 && syndrome === 4'b0000)
-    assign correctCodeWord[0] = ~codeWord[0];
+    correctCodeWord[0] = ~codeWord[0];
 
   end
 
 
 endmodule:makeCorrect
 
-  
+
 module errorCheck
-  (input [12:0] inCode,
-   input [3:0] syndrome,
-   output is1BitErr,
-   output is2BitErr);
+  (input logic [12:0] inCode,
+   input logic [3:0] syndrome,
+   output logic is1BitErr,
+   output logic is2BitErr);
 
   logic isEven, PGfail;
-
-  makeSyndrome DUT (.codeWord(inCode), .syndrome(syndrome));
 
   assign isEven = ^inCode;  //if it is even, PG fails
   assign PGfail = isEven;
@@ -77,37 +74,22 @@ module errorCheck
   always_comb begin
     if (~PGfail && syndrome === 4'b0000)
       begin
-        assign is1BitErr = false;
-        assign is2BitErr = false;
+        assign is1BitErr = 0;
+        assign is2BitErr = 0;
       end
     else if (PGfail) // 1 bit error, either in the PG or normal bit
       begin
-         assign is1BitErr = true;
-         assign is2BitErr = false;
+         assign is1BitErr = 1;
+         assign is2BitErr = 0;
       end
-    else if (~PGfail && syndrome !== 4'b0000):  // 2bits errors
+    else if (~PGfail && syndrome !== 4'b0000)  // 2bits errors
       begin
-        assign is1BitErr = false;
-        assign is2BitErr = true;
+        assign is1BitErr = 0;
+        assign is2BitErr = 1;
     end
   end
 
 endmodule : errorCheck
 
 
-logic [12:0] temp;
 
-makeCorrect MC (.codeWord(temp), .*)
-
-if (inCode[0] === 0 && syndrome === 4'b0000)
-  assign outCode = inCode;
-else if (inCode[0] === 1 && syndrome !== 4'b0000)
-  assign temp = inCode[12:0];
-  assign is1BitErr = 1;
-else if (inCode[0] === 1 && syndrome === 4'b0000)
-  assign outCode = inCode;
-  assign outCode[0] = 0;
-  assign is1BitErr = 1;
-else if (inCode[0] === 0 && syndrome !== 4'b0000)
-  assign is2BitErr = 1
-  assign outCode = inCode;
