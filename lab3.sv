8096785893443ae1f72274c26e78e9035ab6e500
Jean-chalisa <78066597+Jean-chalisa@users.noreply.github.com>
2021-03-07 10:20:40 -0500

    Update lab3.sv

--- lab3.sv
+++ lab3.sv
@@ -1,12 +1,24 @@
 `default_nettype none
 
+`default_nettype none
+
 module SECDEDdecoder (input  logic [12:0] inCode,
                       output logic [3:0] syndrome,
                       output logic is1BitErr, is2BitErr, 
                       output logic [12:0] outCode);
 
 
+makeSyndrome dut1 (.codeWord(inCode), .*);
+
+makeCorrect dut2 ( .codeWord(inCode), .syndrome(syndrome),
+                   .correctCode(corrected));
+
+errorCheck dut2 ( .inCode(.*), .syndrome(.*), .1BitErr(.*), 2BitErr(.*))
+
+
+
 endmodule:  SECDEDdecoder
+
 
 module makeSyndrome (input  logic [12:0] codeWord,
                      output logic [3:0] syndrome);
