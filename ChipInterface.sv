f4ad3f855aa2e5922f9971590333e4751baa2891
Jean-chalisa <78066597+Jean-chalisa@users.noreply.github.com>
2021-03-10 22:58:17 -0500

    Create ChipInterface.sv

---
+++ ChipInterface.sv
@@ -0,0 +1,9 @@
+module ChipInterface
+
+(input logic [17:0] SW,
+output logic [17:0] LEDR,
+output logic [7:0] LEDG,
+output logic [6:0] HEX7, HEX6, HEX5, HEX4,
+HEX3, HEX2, HEX1, HEX0);
+  
+//not done
