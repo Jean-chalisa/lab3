07d00a5452ac9a834d7483c50e349395ddfb2fc9
Jean-chalisa <78066597+Jean-chalisa@users.noreply.github.com>
2021-03-12 15:00:39 -0500

    Update test.sv

--- test.sv
+++ test.sv
@@ -13,14 +13,14 @@ module errorCheck_test();
              "code = %b, syndrome = %b, 1bitErr = %b, 2bitErr = %b",
               inCode, syndrome, is1BitErr, is2BitErr);
 
-    #5 inCode = 13'b0000_0000_0000_0;
-    #5 inCode = 13'b0000_0000_0000_1; //PG is wrong
-    #5 inCode = 13'b0000_0000_0001_1; 
-    #5 inCode = 13'b0000_0000_0001_0;
-    #5 inCode = 13'b0000_0001_1001_0;
-    #5 inCode = 13'b0000_0001_0101_1; //G0
-    #5 inCode = 13'b0000_0001_0101_0; //PG wrong ->2 bits err
-    #5 inCode = 13'b0000_0000_0111_0; //w
+    #5 inCode = 13'b0000_0000_0000_0; // consistent
+    #5 inCode = 13'b0000_0000_0000_1; // one bit error, PG wrong
+    #5 inCode = 13'b0000_0000_0001_1; // 2 bit err
+    #5 inCode = 13'b0000_0000_0001_0; // one bit err, syndrome = 0001
+    #5 inCode = 13'b0000_0001_1001_0; // syndrome = 0000, PG wrong
+    #5 inCode = 13'b0000_0001_0101_1; // G0
+    #5 inCode = 13'b0000_0001_0101_0; // PG wrong ->2 bits err
+    #5 inCode = 13'b1111_1111_1111_1; // all ones , 1 bit error, syndrome = 1100
 
     #20 $finish;
   end
@@ -49,20 +49,18 @@ module SECDEDdecoderTest();
               corrected : %b", 
               inCode, syndrome, is1BitErr, is2BitErr, outCode);
 
-    #5 inCode = 13'b0000_0000_0000_0; 
+    #5 inCode = 13'b0000_0000_0000_0; //consistent
     #5 inCode = 13'b0000_0000_0000_1; //PG is wrong
-    #5 inCode = 13'b0000_0000_0001_1; 
-    #5 inCode = 13'b0000_0000_0001_0;
-    #5 inCode = 13'b0000_0001_1001_0;
+    #5 inCode = 13'b0000_0000_0001_1; //2 bit err -> no change
+    #5 inCode = 13'b0000_0000_0001_0; //one bit err, bit1 flipped
+    #5 inCode = 13'b0000_0001_1001_0; // syndrome = 0000, PG wrong
     
-    #5 inCode = 13'b0000_0001_0101_0; //PG wrong ->2 bits err
-    #5 inCode = 13'b0000_0000_0111_0; 
-    #5 inCode = 13'b1111_1111_1111_1; 
-    #5 inCode = 13'b0000_0000_0101_0;
-
+    #5 inCode = 13'b0000_0001_0101_0; //PG wrong ->2 bits err ->no changes on outcode
+    #5 inCode = 13'b1111_1111_1111_1; //all ones
+    #5 inCode = 13'b0000_0000_0101_0; //consistent
     #5 inCode = 13'b0000_0000_0100_0; //data = 1 
     #5 inCode = 13'b0000_0001_0101_1; //G0
-    #5 inCode = 13'b0000_0000_0000_0;
+
 
 
     #20 $finish;
