87c87c71659825ecbb36330d2cc0c12d0c8d5cbd
Jean-chalisa <78066597+Jean-chalisa@users.noreply.github.com>
2021-03-11 19:38:36 -0500

    Update ChipInterface.sv

--- ChipInterface.sv
+++ ChipInterface.sv
@@ -3,8 +3,8 @@ module ChipInterface
 (input logic [17:0] SW,
 output logic [17:0] LEDR,
 output logic [7:0] LEDG,
-output logic [6:0] HEX7, HEX6, HEX5, HEX4,
-HEX3, HEX2, HEX1, HEX0);
+output logic [6:0] HEX9, HEX8, HEX7, HEX6, HEX5,
+                   HEX4, HEX3, HEX2, HEX1, HEX0);
   
   logic [12:0] inCode;
   logic [12:0] outCode;
@@ -17,13 +17,15 @@ HEX3, HEX2, HEX1, HEX0);
   assign is1BitErr = LEDG[0];
   assign is2BitErr = LEDG[1];
   
+  assign data = {inCode[12], inCode[11], inCode[10],
+                 inCode[9], inCode[7], inCode[6],
+                 inCode[5], inCode[3])
   
   
-//   SevenSegmentControl ssc(.HEX9, .HEX8, .HEX7, .HEX6, .HEX5,
-//                           .HEX4, .HEX3, .HEX2, .HEX1, .HEX0,
-//                           .BCD9, .BCD8, .BCD7, .BCD6, .BCD5,
-//                           .BCD4, .BCD3, .BCD2, .BCD1, .BCD0,
-//                           .turn_on(SW[17:8]));
- 
   
+    
+  SECDEDdecoder dec(.inCode, .syndrome,
+                    .is1BitErr, .is2BitErr, outCode);
+  
+
 //not done
