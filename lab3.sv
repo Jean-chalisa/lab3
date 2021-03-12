a4c30f93ed71fd5c5b1fa90a5a5f6cbabe9d9c25
Jean-chalisa <78066597+Jean-chalisa@users.noreply.github.com>
2021-03-11 21:41:19 -0500

    Update lab3.sv

--- lab3.sv
+++ lab3.sv
@@ -31,6 +31,8 @@ module makeSyndrome (input  logic [12:0] codeWord,
 
   assign syndrome[3] = codeWord[8] ^ codeWord[9] ^ codeWord[10] ^ codeWord[11] ^
                        codeWord[12];
+
+
 endmodule: makeSyndrome
 
 
@@ -39,81 +41,135 @@ module makeCorrect (input  logic [12:0] codeWord,
                     output logic [12:0] correctCodeWord);
 
   logic PGfail;
-  logic [12:0] temp;
 
   assign PGfail = ^codeWord;
 
   always_comb begin
 
-    correctCodeWord = codeWord;
+  correctCodeWord = codeWord;
 
   if (PGfail === 1 && syndrome !== 4'b0000)
     correctCodeWord[syndrome] = ~codeWord[syndrome];
 
   else if (PGfail === 1 && syndrome === 4'b0000)
     correctCodeWord[0] = ~codeWord[0];
-  
+
   end
+
 
 endmodule:makeCorrect
 
-module makeCorrect_test();
-
-  logic [12:0] codeWord, correctCodeWord;
-  logic [3:0] syndrome;
-
-  makeSyndrome t1 (.*);
-  makeCorrect DUT (.*);
-
-  initial begin
-  $monitor($time,,
-            "code = %b, syndrome = %b, correctCodeWord= %b",
-            codeWord, syndrome, correctCodeWord);
-
-  #5 codeWord = 13'b0000_0000_0000_0;
-  #5 codeWord = 13'b0000_0000_0000_1;
-  #5 codeWord = 13'b0000_0000_0001_1;
-  #5 codeWord = 13'b0000_0000_0001_0; //
-  #5 codeWord = 13'b0000_0001_1001_0;
-  #5 codeWord = 13'b0000_0001_0101_1; //G0
-  #5 codeWord = 13'b0000_0001_0101_0; //PG wrong ->2 bits err
-  #5 codeWord = 13'b0000_0000_0111_0; //check again
-
-  #10 $finish;
-  end
-endmodule:makeCorrect_test
-
 
 module errorCheck
+
   (input logic [12:0] inCode,
    input logic [3:0] syndrome,
    output logic is1BitErr,
    output logic is2BitErr);
 
-  logic isEven, PGfail;
+  logic isOdd, GPfail;
 
-  assign isEven = ^inCode;  //if it is even, PG fails
-  assign PGfail = isEven;
+  assign isOdd = ^inCode;  //if it is odd PG is correct
+  assign GPfail = isOdd;  //if it is even, PG fails
 
   always_comb begin
-    if (~PGfail && syndrome === 4'b0000)
+  
+  is1BitErr = 0;
+  is2BitErr = 0;
+  
+    if (~GPfail && syndrome === 4'b0000)
       begin
-        assign is1BitErr = 0;
-        assign is2BitErr = 0;
+        is1BitErr = 0;
+        is2BitErr = 0;
       end
-    else if (PGfail) // 1 bit error, either in the PG or normal bit
+    else if (GPfail) // 1 bit error, either in the GP or normal bit
       begin
-         assign is1BitErr = 1;
-         assign is2BitErr = 0;
+        is1BitErr = 1;
+        is2BitErr = 0;
       end
-    else if (~PGfail && syndrome !== 4'b0000)  // 2bits errors
+    else if (~GPfail && syndrome !== 4'b0000)  // 2bits errors
       begin
-        assign is1BitErr = 0;
-        assign is2BitErr = 1;
+        is1BitErr = 0;
+        is2BitErr = 1;
     end
   end
 
 endmodule : errorCheck
+
+
+module BCDtoSevenSegment (input logic [3:0] bcd,
+                          output logic [6:0] segment);
+
+  always_comb
+    unique case (bcd[3:0])
+      4'b0000: segment = 7'b100_0000;
+      4'b0001: segment = 7'b111_1001;
+      4'b0010: segment = 7'b010_0100;
+      4'b0011: segment = 7'b011_0000;
+      4'b0100: segment = 7'b001_1001;
+      4'b0101: segment = 7'b001_0010;
+      4'b0110: segment = 7'b000_0010;
+      4'b0111: segment = 7'b111_1000;
+      4'b1000: segment = 7'b000_0000;
+      4'b1001: segment = 7'b001_0000;
+      4'b1010: segment = 7'b000_1000;
+      4'b1011: segment = 7'b000_0011;
+      4'b1100: segment = 7'b100_0110;
+      4'b1101: segment = 7'b010_0001;
+      4'b1110: segment = 7'b000_0110;
+      4'b1111: segment = 7'b000_1110;
+    endcase
+endmodule: BCDtoSevenSegment
+
+module SevenSegmentDigit (input logic [3:0] bcd,
+                          output logic [6:0] segment, 
+                          input logic blank);
+  logic [6:0] decoded;
+  
+  BCDtoSevenSegment b2ss(.bcd(bcd), .segment(decoded)); 
+  // obtain for a specific digit
+  always_comb begin
+    if (blank)
+      segment = 7'b111_1111; // want blanks and 1 is off
+    else
+      segment = decoded;
+  end
+
+endmodule: SevenSegmentDigit
+
+module SevenSegmentControl
+  (output logic [6:0] HEX9, HEX8, HEX7, HEX6, HEX5,
+   output logic [6:0] HEX4, HEX3, HEX2, HEX1, HEX0, 
+   input logic [3:0] BCD9, BCD8, BCD7, BCD6, BCD5, 
+   input logic [3:0] BCD4, BCD3, BCD2, BCD1, BCD0);
+  
+
+  logic turn_on;
+  assign turn_on = 0;
+  // instantiate for 10 digits
+  SevenSegmentDigit SSD0 (.bcd(BCD0), .segment(HEX0), .blank(turn_on));
+  SevenSegmentDigit SSD1 (.bcd(BCD1), .segment(HEX1), .blank(turn_on));
+  SevenSegmentDigit SSD2 (.bcd(BCD2), .segment(HEX2), .blank(turn_on));
+  SevenSegmentDigit SSD3 (.bcd(BCD3), .segment(HEX3), .blank(turn_on));
+  SevenSegmentDigit SSD4 (.bcd(BCD4), .segment(HEX4), .blank(~turn_on));
+  SevenSegmentDigit SSD5 (.bcd(BCD5), .segment(HEX5), .blank(~turn_on));
+  SevenSegmentDigit SSD6 (.bcd(BCD6), .segment(HEX6), .blank(turn_on));
+  SevenSegmentDigit SSD7 (.bcd(BCD7), .segment(HEX7), .blank(turn_on));
+  SevenSegmentDigit SSD8 (.bcd(BCD8), .segment(HEX8), .blank(turn_on));
+  SevenSegmentDigit SSD9 (.bcd(BCD9), .segment(HEX9), .blank(turn_on));
+
+endmodule: SevenSegmentControl
+
+
+
+
+  
+
+
+
+
+
+   
 
 
 
