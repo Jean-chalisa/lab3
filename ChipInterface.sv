1ce7a1c0c9873bee6dc06ab80653885454f5a7d8
Jean-chalisa <78066597+Jean-chalisa@users.noreply.github.com>
2021-03-11 19:46:11 -0500

    Update ChipInterface.sv

--- ChipInterface.sv
+++ ChipInterface.sv
@@ -13,19 +13,22 @@ output logic [6:0] HEX9, HEX8, HEX7, HEX6, HEX5,
   
   assign inCode = SW[12:0];
   assign outCode = LEDR[12:0]; //red LED
-  assign syndrome = LEDG[7:4];
+  assign syndrome = LEDG[7:4]; //green LED
   assign is1BitErr = LEDG[0];
   assign is2BitErr = LEDG[1];
   
-  assign data = {inCode[12], inCode[11], inCode[10],
+  assign inData = {inCode[12], inCode[11], inCode[10],
                  inCode[9], inCode[7], inCode[6],
-                 inCode[5], inCode[3])
+                   inCode[5], inCode[3]};
   
-  
-  
-    
+  assign outData = {outCode[12], outCode[11], outCode[10],
+                    outCode[9], outCode[7], outCode[6],
+                    outCode[5], outCode[3]);
+
   SECDEDdecoder dec(.inCode, .syndrome,
                     .is1BitErr, .is2BitErr, outCode);
+                 
+  SevenSegmentControl
   
 
 //not done
